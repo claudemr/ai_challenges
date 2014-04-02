87a948baa4095be65c4fb72f3601c07cc0ef0a00
claudemr <claudemr@live.fr>
2014-04-02 13:58:21 +0200

    [TRON] Initial revision

---
+++ tron/main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    // Read init information from standard input, if any
+
+    while (1)
+    {
+        // Read information from standard input
+        int n;
+        scanf("%d", &n);
+
+        // Compute logic here
+
+        // fprintf(stderr, "Debug messages...\n");
+
+        // Write action to standard output
+        printf("LEFT\n");
+    }
+
+    return 0;
+}
